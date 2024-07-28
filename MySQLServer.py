c4d0d595d95c2472e9ada80104bc4ad78e175405
frankassafuah <frankassafuah@gmail.com>
2024-07-28 19:27:58 +0000

    handling database connections and errors

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,5 +1,4 @@
 import mysql.connector
-from mysql.connector import Error
 
 def create_database():
     try:
@@ -15,8 +14,8 @@ def create_database():
             cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
             print("Database 'alx_book_store' created successfully!")
     
-    except Error as e:
-        print(f"Error: '{e}'")
+    except mysql.connector.Error:
+        print(f"Error: '{mysql.connector.Error}'")
     
     finally:
         if connection.is_connected():

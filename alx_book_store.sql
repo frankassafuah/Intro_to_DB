04cd2d1d862fcaef10eb752917949fb619f7075d
frankassafuah <frankassafuah@gmail.com>
2024-07-28 19:17:25 +0000

    database schema for your online bookstore.

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,8 +1,8 @@
 -- Create the database
-CREATE DATABASE alx_book_store;
+CREATE DATABASE IF NOT EXISTS alx_book_store
 
 -- Use the database
-USE alx_book_store;
+USE alx_book_store
 
 -- Create the Authors table
 CREATE TABLE Authors (
